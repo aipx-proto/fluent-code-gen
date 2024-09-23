bff5c6191f42ba881324f411d38834e8d7770012
Chauqni Sun <chuanqi.sun.dev@gmail.com>
2024-09-22 19:20:49 -0700

    relative redirect

--- src/lib/azure.ts
+++ src/lib/azure.ts
@@ -4,7 +4,7 @@ import { AzureOpenAI } from "openai";
 const credential = new InteractiveBrowserCredential({
   tenantId: "72f988bf-86f1-41af-91ab-2d7cd011db47",
   clientId: "6e4c509e-2b65-40dc-b461-beb2c824f63a",
-  // redirectUri: `${location.origin}/blank.html`,
+  redirectUri: location.href,
   loginStyle: "redirect",
 });
 
