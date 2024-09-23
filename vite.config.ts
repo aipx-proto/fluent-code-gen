6a6b020db97763f5a8eb6bdeb9e364d482e0b122
Chauqni Sun <chuanqi.sun.dev@gmail.com>
2024-09-22 19:16:46 -0700

    fix app prefix

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: `/fluent-code-gen/`, // This is hard coded for the GitHub Pages deployment. See https://vitejs.dev/guide/static-deploy#github-pages
+});
